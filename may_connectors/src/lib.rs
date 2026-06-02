7841ba8823b8f84192c5cb25e4267e300172b532
Christian Edensor Arbon <101029381+christiannarbon@users.noreply.github.com>
2026-06-02 18:28:41 +0900

    Updating the directory managment for the connectors

--- may_connectors/src/lib.rs
+++ may_connectors/src/lib.rs
@@ -1,11 +1,11 @@
+pub mod dwh;
 pub mod error;
 pub mod models;
-pub mod postgres;
 pub mod registry;
 pub mod traits;
 
+pub use dwh::postgres::PostgresConnector;
 pub use error::ConnectorError;
 pub use models::{ColumnValue, QueryResult, Row};
-pub use postgres::PostgresConnector;
 pub use registry::ConnectorRegistry;
 pub use traits::WarehouseConnector;
