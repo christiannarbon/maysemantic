2545244e85451a8dcac7c24a3a2d7649a8a3f9f7
Christian Edensor Arbon <101029381+christiannarbon@users.noreply.github.com>
2026-06-09 22:05:00 +0900

    Adding dedicated test for the chasm trap

---
+++ may_core/tests/unit/compiler/chasm_trap_test.rs
@@ -0,0 +1,26 @@
+use may_core::ast::{SqlNode, TableIdent};
+use may_core::compiler::{ChasmTrapError, ChasmTrapHandler, PathClassification};
+
+#[test]
+fn test_inject_ctes_single_fact() {
+    let query = SqlNode::Table(TableIdent("orders".to_string()));
+    let classification = PathClassification::SingleFact;
+    let result = ChasmTrapHandler::inject_ctes(query.clone(), &classification, "user_id");
+    assert_eq!(result.unwrap(), query);
+}
+
+#[test]
+fn test_inject_ctes_pure_dimension() {
+    let query = SqlNode::Table(TableIdent("users".to_string()));
+    let classification = PathClassification::PureDimension;
+    let result = ChasmTrapHandler::inject_ctes(query.clone(), &classification, "user_id");
+    assert_eq!(result.unwrap(), query);
+}
+
+#[test]
+fn test_inject_ctes_empty_fact_tables() {
+    let query = SqlNode::Table(TableIdent("orders".to_string()));
+    let classification = PathClassification::MultiFactJoin { fact_tables: vec![] };
+    let result = ChasmTrapHandler::inject_ctes(query, &classification, "user_id");
+    assert_eq!(result.unwrap_err(), ChasmTrapError::EmptyFactTableList);
+}
