7ed8517a0fce42d2541355897d9c82366c5c7199
Christian Edensor Arbon <101029381+christiannarbon@users.noreply.github.com>
2026-06-10 05:34:31 +0900

    Adding test to the ctronller

--- may_core/tests/unit/main.rs
+++ may_core/tests/unit/main.rs
@@ -28,6 +28,8 @@ pub mod models {
     pub mod aggregation_test;
     #[path = "state_test.rs"]
     pub mod state_test;
+    #[path = "entity_type_test.rs"]
+    pub mod entity_type_test;
 }
 
 pub mod compiler {
