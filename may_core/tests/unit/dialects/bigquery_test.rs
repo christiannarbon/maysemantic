9213965e39fd017f0dd25bf6bc0be2d6ac1a0e2f
Christian Edensor Arbon <101029381+christiannarbon@users.noreply.github.com>
2026-06-29 19:03:47 +0900

    create test for BQ dialect on qualified tables and columns

--- may_core/tests/unit/dialects/bigquery_test.rs
+++ may_core/tests/unit/dialects/bigquery_test.rs
@@ -269,7 +269,9 @@ fn test_bigquery_dialect_write_cast() {
 fn test_bigquery_schema_qualified_table() {
     let ast = SqlNode::Query {
         ctes: None,
-        select: Box::new(SqlNode::Select(vec![Expr::Column(ColumnIdent("id".to_string()))])),
+        select: Box::new(SqlNode::Select(vec![Expr::Column(ColumnIdent(
+            "id".to_string(),
+        ))])),
         from: Box::new(SqlNode::From {
             source: Box::new(SqlNode::Table(TableIdent("public.users".to_string()))),
             joins: vec![],
@@ -287,7 +289,9 @@ fn test_bigquery_schema_qualified_table() {
 fn test_bigquery_qualified_column() {
     let ast = SqlNode::Query {
         ctes: None,
-        select: Box::new(SqlNode::Select(vec![Expr::Column(ColumnIdent("users.id".to_string()))])),
+        select: Box::new(SqlNode::Select(vec![Expr::Column(ColumnIdent(
+            "users.id".to_string(),
+        ))])),
         from: Box::new(SqlNode::From {
             source: Box::new(SqlNode::Table(TableIdent("users".to_string()))),
             joins: vec![],
