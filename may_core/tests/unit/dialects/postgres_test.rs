4e85f2648a40fccf8b8c3bb26f46f66960071571
Christian Edensor Arbon <101029381+christiannarbon@users.noreply.github.com>
2026-07-01 00:19:58 +0900

    update sort direction test in pg dialect

--- may_core/tests/unit/dialects/postgres_test.rs
+++ may_core/tests/unit/dialects/postgres_test.rs
@@ -402,7 +402,9 @@ fn test_postgres_order_by() {
     use may_core::ast::{OrderByExpr, SortDirection};
     let ast = SqlNode::Query {
         ctes: None,
-        select: Box::new(SqlNode::Select(vec![Expr::Column(ColumnIdent("name".to_string()))])),
+        select: Box::new(SqlNode::Select(vec![Expr::Column(ColumnIdent(
+            "name".to_string(),
+        ))])),
         from: Box::new(SqlNode::From {
             source: Box::new(SqlNode::Table(TableIdent("users".to_string()))),
             joins: vec![],
