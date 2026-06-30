e7213c63d121c058b32cd16321817b47433b2c18
Christian Edensor Arbon <101029381+christiannarbon@users.noreply.github.com>
2026-06-30 22:09:30 +0900

    reflect ordering implementation on compiler lowering tests

--- may_core/tests/unit/compiler/lowering_test.rs
+++ may_core/tests/unit/compiler/lowering_test.rs
@@ -247,6 +247,9 @@ mod lowering_tests {
                 dimension: "region".to_string(),
             }]))),
             having: None,
+        order_by: vec![],
+        limit: None,
+        offset: None,
         };
 
         let lowered_query = lowering.lower_node(query).expect("lowering failed");
