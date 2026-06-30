30b320f106f4729051b8fe22a53d42286d113b2f
Christian Edensor Arbon <101029381+christiannarbon@users.noreply.github.com>
2026-06-30 21:33:26 +0900

    Qualified join column test on PG

--- may_core/tests/unit/dialects/postgres_test.rs
+++ may_core/tests/unit/dialects/postgres_test.rs
@@ -340,7 +340,9 @@ fn test_postgres_unnest_unsupported() {
 fn test_postgres_join_on_qualified_columns() {
     let ast = SqlNode::Query {
         ctes: None,
-        select: Box::new(SqlNode::Select(vec![Expr::Column(ColumnIdent("users.id".to_string()))])),
+        select: Box::new(SqlNode::Select(vec![Expr::Column(ColumnIdent(
+            "users.id".to_string(),
+        ))])),
         from: Box::new(SqlNode::From {
             source: Box::new(SqlNode::Table(TableIdent("users".to_string()))),
             joins: vec![SqlNode::Join {
