5a9c01b3ee36d569ab05b74a6ee470e896f89adb
Christian Edensor Arbon <101029381+christiannarbon@users.noreply.github.com>
2026-07-01 22:53:23 +0900

    adding pagination support for mod

--- may_core/src/ast/mod.rs
+++ may_core/src/ast/mod.rs
@@ -2,6 +2,8 @@ pub mod builder;
 pub mod combiners;
 pub mod node;
 
-pub use builder::{build_semantic_group_by, build_semantic_select, build_semantic_timespine_query, with_pagination};
+pub use builder::{
+    build_semantic_group_by, build_semantic_select, build_semantic_timespine_query, with_pagination,
+};
 pub use combiners::{and, eq, literal_bool, literal_num, literal_str, or};
 pub use node::{ColumnIdent, Expr, JoinType, OrderByExpr, SortDirection, SqlNode, TableIdent};
