fd58a05ce99d2f1e041ce84c065afd8d1be4083d
Christian Edensor Arbon <101029381+christiannarbon@users.noreply.github.com>
2026-07-04 20:03:45 +0900

    add model to dimension and measure reference

--- may_core/src/dialects/core.rs
+++ may_core/src/dialects/core.rs
@@ -254,10 +254,18 @@ pub trait SqlDialect: std::fmt::Debug + Send + Sync {
             Expr::Cast { expr, target_type } => self.write_cast_expr(buf, expr, target_type),
             Expr::JsonAccess { column, path } => self.write_json_access(buf, column, path),
             Expr::Unnest { expr } => self.write_unnest_expr(buf, expr),
-            Expr::DimensionRef { model: _, entity, dimension } => Err(DialectError::UnsupportedExpr(
-                format!("DimensionRef({entity}.{dimension})"),
-            )),
-            Expr::MeasureRef { model: _, entity, measure } => Err(DialectError::UnsupportedExpr(format!(
+            Expr::DimensionRef {
+                model: _,
+                entity,
+                dimension,
+            } => Err(DialectError::UnsupportedExpr(format!(
+                "DimensionRef({entity}.{dimension})"
+            ))),
+            Expr::MeasureRef {
+                model: _,
+                entity,
+                measure,
+            } => Err(DialectError::UnsupportedExpr(format!(
                 "MeasureRef({entity}.{measure})"
             ))),
             Expr::Raw(sql) => {
